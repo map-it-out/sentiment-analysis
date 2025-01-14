b0fedb10e21e437b84ce548d4428c5a16d7589c0
Glenn Steven Santoso <glennstevencg21@gmail.com>
2025-01-14 22:18:00 +0700

    fix: service account for sheet editor

--- src/utils/sheets/sheets_auth.py
+++ src/utils/sheets/sheets_auth.py
@@ -1,7 +1,5 @@
 import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
@@ -11,27 +9,15 @@ SCOPES = [
 ]
 
 def get_credentials():
-    """Gets valid user credentials from storage.
+    """Gets valid service account credentials.
     
     Returns:
         Credentials, the obtained credential.
     """
-    creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-    
-    return creds
+    return Credentials.from_service_account_file(
+        "credentials.json",
+        scopes=SCOPES
+    )
 
 def get_sheets_service():
     """Creates and returns Google Sheets API service instance."""
